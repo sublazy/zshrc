1464bd20dbf0bcfea0f0c0f4f3934957a6734c18
Cezary Sałbut <csalbut@gmail.com>
2018-07-22 01:26:23 +0200

    add separate aliases file

---
+++ .zsh_custom.d/aliases.zsh
@@ -0,0 +1,18 @@
+alias git="git --no-pager"
+alias vc="vcsh"
+alias egrep="egrep --color=auto"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ls="ls --color=auto --human-readable --group-directories-first"
+alias ll="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+
+alias zf='fasd -f'        # file
+alias zd='fasd -d'        # directory
+alias za='fasd -a'        # any
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zvim='fasd -f -e nvim'
+alias zop='fasd -f -e xdg-open'
